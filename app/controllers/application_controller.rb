981c68943f52cf703f17ee9381fe93992c1c7cb2
Pavel Osetrov <pavel.osetrov@me.com>
2015-12-22 20:46:44 +0300

    Регистрация / авторизация пользователей и home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate_user!
+
+  def require_authorization
+    redirect_to new_user_session_path
+  end
 end
